68d4d60f40f67d673da3b8e07eb672fa137ed25a
Kory Asta <korysao125@gmail.com>
2022-01-19 16:17:23 -0600

    Homework 1

--- TravelManager.java
+++ TravelManager.java
@@ -1,6 +1,8 @@
 public class TravelManager {
     public static void main(String[] args){
-        String flightInfo = Flight.toString();
-        System.out.println(flightInfo);
+        Airline comp = Airline.toString();
+        Airport city = Airport.toString();
+        Flight flightInfo = Flight.toString();
+        System.out.println('Airline: '+ comp + '\nAirport: '+ city + '\nFlight: '+ flightInfo);
     }
 }
